5ff78e33704ac2b7955bc01e3fd6d293a9b2bd50
RoshanMuhammedR <roshanmuhammed50@gmail.com>
2024-01-11 22:09:49 +0530

    string

---
+++ string.js
@@ -0,0 +1,16 @@
+let string1 = "Roshan"
+let string2 = "muhammedR"
+
+console.log("Hello My name is "+" "+string1+" "+string2); //not a good practice
+
+console.log(`Hello My name is ${string1} ${string2}`); //recomended
+
+let emailId = "roshanmuhammed50@gmail.com" //string
+
+console.log(emailId.length);
+console.log(emailId.charAt[0]);
+console.log(emailId[0]);
+console.log(emailId.split("@"))
+console.log(emailId.replace("50","20"));
+console.log(emailId.replace("70","20"));
+console.log(emailId.toUpperCase());
