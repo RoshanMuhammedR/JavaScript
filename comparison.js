9b45d413c07991209cb316e9028b853700594e35
RoshanMuhammedR <roshanmuhammed50@gmail.com>
2024-01-11 20:10:23 +0530

    stack and heap added tp comp.

--- comparison.js
+++ comparison.js
@@ -6,3 +6,20 @@ console.log(a>b);//return bool
 >  <  == !=  <=  >= etc...
  */
 
+let myName = "roshan"
+let nameCopy = myName;
+nameCopy = "muhammed"
+console.log(myName);
+console.log(nameCopy);
+
+
+let obj = {
+    rollNo:1,
+    Name:"roshan"
+}
+
+let obj1= obj
+
+obj1.Name = "muhammed"
+
+console.log(obj.Name);
